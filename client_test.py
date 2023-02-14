47c504193682da51184695a23bd07b49f7b10c35
C190194 <73808832+C190194@users.noreply.github.com>
2023-02-14 23:07:26 +0800

    test wifi disconnection

---
+++ client_test.py
@@ -0,0 +1,29 @@
+import socket
+import cv2
+import pickle
+import struct
+import math
+import time
+import os
+import glob
+
+# Client socket
+# create an INET, STREAMing socket : 
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# host_ip = '<localhost>'# Standard loopback interface address (localhost)
+host_ip = '192.168.3.3' # put the ip of the server here
+port = 10050 # Port to listen on (non-privileged ports are > 1023)
+# now connect to the web server on the specified port number
+client_socket.connect((host_ip,port)) 
+# client_socket.settimeout(5)
+
+try:
+    while True:
+        # client_socket.send(b"Hi from client")
+        msg = client_socket.recv(1024).decode("utf-8")
+        print(msg)
+        time.sleep(5)
+except Exception as e:
+    print(e)
+    print("Client socket closed")
+    client_socket.close()
